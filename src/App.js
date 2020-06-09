7a3cee51ae6f24da4f57db387eff13e84d47a836
Nauman Munir <naumanmunir@gmail.com>
2020-06-09 18:48:43 +0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Dinner from "./Dinner";
+import "./Styles.css";
 
 function App() {
   return (
     <div className="App">
-      <Dinner dishName='Chiken Karhai' sweetDish='Kheer' />
+      <Dinner savory="Chiken Karhai" sweet="Qulab Jamun" />
+      <hr className="darkLine-css"></hr>
+      <Dinner savory="Chiken Qourma" sweet="Kheer" />
+      <hr className="darkLine-css"></hr>
+      <Dinner savory="Nihari" sweet="Gajar ka Halwa" />
     </div>
   );
 }
--- src/Dinner.js
+++ src/Dinner.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./Styles.css";
 
-function Dinner({dishName, sweetDish}) {
+function Dinner({ savory, sweet }) {
   return (
     <div className="dish-css">
-      <h1>Today we are serving {dishName}</h1>
-      <h1>Today we are serving {sweetDish}</h1>
+      <h1>Today we are serving {savory}</h1>
+      <h1>Today we are serving {sweet}</h1>
     </div>
   );
 }
 
 export default Dinner;
-
